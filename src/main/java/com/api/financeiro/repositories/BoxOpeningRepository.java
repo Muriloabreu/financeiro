13d3897e3d7e06ad373ea0d07a9a77e5f7ae6db5
muriloabreu <murilo.lilo@hotmail.com>
2023-05-08 20:24:34 -0300

    Criação de Interface BoxOpeningRepository

---
+++ src/main/java/com/api/financeiro/repositories/BoxOpeningRepository.java
@@ -0,0 +1,9 @@
+package com.api.financeiro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.financeiro.models.BoxOpeningModel;
+
+public interface BoxOpeningRepository extends JpaRepository<BoxOpeningModel, Long> {
+
+}
