7e347e95877ce154c576c426fba3ded25cbc1a3b
muriloabreu <murilo.lilo@hotmail.com>
2023-05-08 20:20:48 -0300

    Implementação e correção de metodo

--- src/main/java/com/api/financeiro/repositories/CompanyRepository.java
+++ src/main/java/com/api/financeiro/repositories/CompanyRepository.java
@@ -1,12 +1,11 @@
 package com.api.financeiro.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.api.financeiro.models.CompanyModel;
 
 public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
 	
-	boolean existsByName(String corporate_name );
+	
 	boolean existsByCnpj(String cnpj);
 
 }
