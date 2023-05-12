c49d476570aeef7f950793aefd5755287d4c63ad
muriloabreu <murilo.lilo@hotmail.com>
2023-05-11 21:12:10 -0300

    Criação de método na interface ExpenseService para buscar ID da classe BoxOpeningModel

--- src/main/java/com/api/financeiro/services/ExpenseService.java
+++ src/main/java/com/api/financeiro/services/ExpenseService.java
@@ -3,6 +3,7 @@ package com.api.financeiro.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.api.financeiro.models.BoxOpeningModel;
 import com.api.financeiro.models.ExpenseModel;
 
 
@@ -11,6 +12,7 @@ public interface ExpenseService {
 	
 	List<ExpenseModel> findAll();
 	Optional<ExpenseModel> findById(Long id);
+	Optional<BoxOpeningModel>findByIdBox(Long id);
 	ExpenseModel save(ExpenseModel expense);
 	void delete(ExpenseModel expense);
 
