f4424718399b85a45e9a1519e31521ce16f8b1a9
muriloabreu <murilo.lilo@hotmail.com>
2023-05-13 10:18:44 -0300

    Criação de método de busca id na tabela Company

--- src/main/java/com/api/financeiro/services/BranchService.java
+++ src/main/java/com/api/financeiro/services/BranchService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.financeiro.models.BranchModel;
+import com.api.financeiro.models.CompanyModel;
 
 public interface BranchService {
 	
 	List<BranchModel> findAll();
 	Optional<BranchModel> findById(Long id);
+	Optional<CompanyModel>findByIdCompany(Long id);
 	BranchModel save(BranchModel branch);
 	void delete (BranchModel brance);
 	boolean existsByName(String name);
